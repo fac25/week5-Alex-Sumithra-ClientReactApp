ca037b341406e57b7c71376c0a9386791395438e
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-10-20 11:08:56 +0100

    Created username input

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,11 +1,19 @@
 import Board from "./Board";
-
+import NameInput from "./NameInput";
+import { useState } from "react";
 
 export default function Game() {
+  const [names, setNames] = useState(Array(2).fill(""));
 
   return (
     <div>
-      <Board/>
+      {names[0] === "" ? (
+        <NameInput setNames={setNames} names={names} />
+      ) : (
+        <>
+          <Board />
+        </>
+      )}
     </div>
   );
 }
