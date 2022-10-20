e96b84f151d0cb4455a209e3c9dd55e03ca3a58c
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-10-20 14:42:33 +0100

    Added tie and scoreboard

---
+++ src/Components/Scoreboard.jsx
@@ -0,0 +1,14 @@
+export default function Scoreboard({ players, score }) {
+  return (
+    <>
+      <div className="headings">
+        <h3>{players[0]}</h3>
+        <h3>Tie</h3>
+        <h3>{players[1]}</h3>
+      </div>
+      <p>{score[0]}</p>
+      <p>{score[1]}</p>
+      <p>{score[2]}</p>
+    </>
+  );
+}
--- src/Components/Square.jsx
+++
@@ -1,7 +0,0 @@
-// export default function Square({ handleClick, value }) {
-//   return (
-//     <button className="square" onClick={handleClick}>
-//       {value}
-//     </button>
-//   );
-// }
